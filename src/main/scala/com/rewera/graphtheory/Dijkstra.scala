bad82104b4e928831d0d7de629fbe958bb8dafec
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2021-07-07 20:57:47 +0100

    Implement Dijkstra's Shortest Path algorithm returning path

--- src/main/scala/com/rewera/graphtheory/Dijkstra.scala
+++ src/main/scala/com/rewera/graphtheory/Dijkstra.scala
@@ -36,7 +36,13 @@ object Dijkstra {
     println(dijkstraOptimised(theGraph_2, 0, 7).mkString("[", ", ", "]"))
     println(dijkstraOptimised(theGraph_2, 0, 2).mkString("[", ", ", "]"))
 
+    println(dijkstraPath(theGraph, 0, 4).mkString("[", ", ", "]"))
+    println(dijkstraPath(theGraph_2, 0, 7).mkString("[", ", ", "]"))
 
+    println()
+    dijkstraTracked(theGraph, 0).foreach(println)
+    println()
+    dijkstraTracked(theGraph_2, 0).foreach(println)
   }
 
   def dijkstra(graph: WeightedGraph, fromNode: Int): Seq[Int] = {
@@ -100,11 +106,79 @@ object Dijkstra {
     distances.flatten.toSeq
   }
 
-  def dijkstraPath(graph: WeightedGraph, fromNode: Int, toNode: Int): Seq[Int] = ???
+  def dijkstraPath(graph: WeightedGraph, fromNode: Int, toNode: Int): Seq[Int] = {
+    val visited = Array.fill(graph.size)(false)
+    val distances = Array.fill[Option[Int]](graph.size)(None)
+    val previousNodes = Array.fill[Option[Int]](graph.size)(None)
+    val queue = new mutable.PriorityQueue()(Ordering.by[(Int, Int), Int](_._2).reverse)
+
+    distances(fromNode) = Some(0)
+    queue.enqueue((fromNode, 0))
+
+    while (queue.nonEmpty) {
+      val (currNode, currNodeDist) = queue.dequeue()
+      visited(currNode) = true
+
+      if (distances(currNode).exists(_ >= currNodeDist)) {
+
+        for (edge <- graph(currNode)) {
+          if (!visited(edge.targetNode)) {
+
+            val newDist = currNodeDist + edge.weight
+            if (!distances(edge.targetNode).exists(_ < newDist)) {
+              distances(edge.targetNode) = Some(newDist)
+              previousNodes(edge.targetNode) = Some(currNode)
+              queue.enqueue((edge.targetNode, newDist))
+            }
+
+            if (currNode == toNode)
+              return reconstructPath(toNode).reverse
+          }
+        }
+      }
+    }
+
+    def reconstructPath(currNode: Int): Seq[Int] = previousNodes(currNode) match {
+      case None               => currNode +: Nil
+      case Some(previousNode) => currNode +: reconstructPath(previousNode)
+    }
+
+    reconstructPath(toNode).reverse
+  }
 
   case class Path(nodes: Seq[Int], totalWeight: Int)
 
-  def dijkstraTracked(graph: WeightedGraph, fromNode: Int): Seq[Path] = ???
+  def dijkstraTracked(graph: WeightedGraph, fromNode: Int): Seq[Path] = {
+    val visited = Array.fill(graph.size)(false)
+    val paths = Array.fill[Option[Path]](graph.size)(None)
+    val queue = new mutable.PriorityQueue()(Ordering.by[(Int, Int), Int](_._2).reverse)
 
+    paths(fromNode) = Some(Path(Seq(fromNode), 0))
+    queue.enqueue((fromNode, 0))
+
+    while (queue.nonEmpty) {
+      val (currNode, currNodeDist) = queue.dequeue()
+      visited(currNode) = true
+
+      if (paths(currNode).exists(_.totalWeight >= currNodeDist)) {
+
+        for (edge <- graph(currNode)) {
+          if (!visited(edge.targetNode)) {
+
+            val newPathOpt = paths(currNode).map { path =>
+              Path(edge.targetNode +: path.nodes, path.totalWeight + edge.weight)
+            }
+
+            for (newPath <- newPathOpt if !paths(edge.targetNode).exists(_.totalWeight < newPath.totalWeight)) {
+              paths(edge.targetNode) = newPathOpt
+              queue.enqueue((edge.targetNode, newPath.totalWeight))
+            }
+          }
+        }
+      }
+    }
+
+    paths.map { case Some(path) => path.copy(nodes = path.nodes.reverse) }
+  }
 
 }
